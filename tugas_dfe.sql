491ec63785c10b944b2c5c7a8ee4ef454621bc48
naufalfirman2023 <emailfleksible11@gmail.com>
2023-10-22 02:30:11 +0700

    second commit

---
+++ tugas_dfe.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Okt 2023 pada 15.57
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugas_dfe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2023_10_13_094937_produk', 1),
+(2, '2023_10_13_150059_pengurus', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengurus`
+--
+
+CREATE TABLE `pengurus` (
+  `id_pengurus` bigint(20) UNSIGNED NOT NULL,
+  `nama_pengurus` varchar(255) NOT NULL,
+  `about` varchar(255) NOT NULL,
+  `npm` varchar(255) NOT NULL,
+  `img` char(20) NOT NULL,
+  `link_ig` char(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `pengurus`
+--
+
+INSERT INTO `pengurus` (`id_pengurus`, `nama_pengurus`, `about`, `npm`, `img`, `link_ig`) VALUES
+(1, 'naufal firmansyah', '@nauffalalalala_', '5210411166', 'nopal.png', 'https://www.instagram.com/nauffalalalala_/?hl=id'),
+(2, 'zaedar ghazalba', '@godhokmiee', '5210411192', 'zaedar.png', 'https://www.instagram.com/godhokmiee/?hl=id'),
+(3, 'bella triana', '@bellatr__', '5210411175', 'bella.png', 'https://www.instagram.com/bellatr__/?hl=id');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga_produk` int(255) NOT NULL,
+  `desk` varchar(255) NOT NULL,
+  `waktu` char(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`id`, `nama_produk`, `harga_produk`, `desk`, `waktu`) VALUES
+(1, 'Coding ', 100000, 'PHP,Python,Android kotlin,java,Web', '1 minggu'),
+(2, 'Paper', 300000, 'Artikel,Jurnal,Review,Proposal,Laporan', '1 minggu'),
+(3, 'Tugas Besar', 2500000, 'Skripsi,Tugas akhir,LTA,', '1 bulan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengurus`
+--
+ALTER TABLE `pengurus`
+  ADD PRIMARY KEY (`id_pengurus`),
+  ADD UNIQUE KEY `pengurus_npm_unique` (`npm`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengurus`
+--
+ALTER TABLE `pengurus`
+  MODIFY `id_pengurus` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
